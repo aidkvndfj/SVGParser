c71e87840acb83cccec5b05aa435958d08051e99
Eric Morse <ericm7597@gmail.com>
2022-02-16 03:13:22 +0000

    updated ignore and parser

--- src/testMain.c
+++ src/testMain.c
@@ -6,14 +6,10 @@ int main(void) {
     SVG* SVG1;
     SVG1 = createSVG("beer.svg");
 
-    // char* svgStr;
-    // svgStr = SVGToString(SVG1);
-    // printf("%s", svgStr);
-    // free(svgStr);
-
-    // printf("%d\n", getLength(getGroups(SVG1)));
-    printf("%d\n", numAttr(SVG1));
-    // printf("%d\n", numGroupsWithLen(SVG1, 1.0));
+    char* svgStr;
+    svgStr = SVGToString(SVG1);
+    printf("%s", svgStr);
+    free(svgStr);
 
     deleteSVG(SVG1);
 
